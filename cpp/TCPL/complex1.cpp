ec132862e698c6820716b48853dd9e70dde1b740
熊鑫 <1781189926@qq.com>
2022-02-11 19:17:28 +0800

    some code

--- cpp/TCPL/complex1.cpp
+++ cpp/TCPL/complex1.cpp
@@ -1,28 +1,28 @@
 class complex {
-    double re, im;
+  double re, im;
 
-   public:
-    complex(double r, double i) : re{r}, im{i} {}
-    complex(double r) : re{r}, im{0} {}
-    complex() : re{0}, im{0} {}
+public:
+  complex(double r, double i) : re{r}, im{i} {}
+  complex(double r) : re{r}, im{0} {}
+  complex() : re{0}, im{0} {}
 
-    double real() const { return re; }
-    void real(double d) { re = d; }
-    double imag() const { return im; }
-    void imag(double d) { im = d; }
+  double real() const { return re; }
+  void real(double d) { re = d; }
+  double imag() const { return im; }
+  void imag(double d) { im = d; }
 
-    complex& operator+=(complex z) {
-        re += z.re, im += z.im;
-        return *this;
-    }
+  complex &operator+=(complex z) {
+    re += z.re, im += z.im;
+    return *this;
+  }
 
-    complex& operator-=(complex z) {
-        re -= z.re, im -= z.im;
-        return *this;
-    }
+  complex &operator-=(complex z) {
+    re -= z.re, im -= z.im;
+    return *this;
+  }
 
-    complex& operator*=(complex);
-    complex& operator/=(complex);
+  complex &operator*=(complex);
+  complex &operator/=(complex);
 };
 
 complex operator+(complex a, complex b) { return a += b; }
@@ -32,7 +32,7 @@ complex operator*(complex a, complex b) { return a *= b; }
 complex operator/(complex a, complex b) { return a /= b; }
 
 bool operator==(complex a, complex b) {
-    return a.real() == b.real() && b.imag() == b.imag();
+  return a.real() == b.real() && b.imag() == b.imag();
 }
 
 bool operator!=(complex a, complex b) { return !(a == b); }
