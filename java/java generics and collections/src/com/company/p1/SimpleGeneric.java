5e55d17b9621751ef110002e9fc9594eda1fb22c
xiongxin <1781189926@qq.com>
2022-03-07 00:14:14 +0800

    some codes

--- java/java generics and collections/src/com/company/p1/SimpleGeneric.java
+++ java/java generics and collections/src/com/company/p1/SimpleGeneric.java
@@ -1,2 +1,31 @@
-package com.company.p1;public class SimpleGeneric {
+package com.company.p1;
+
+public class SimpleGeneric<T> {
+    T t;
+
+    SimpleGeneric(T t) {
+        this.t = t;
+    }
+
+    void erased(T t) {
+        // 无法使用泛型创建对象
+        // T nt = new T();
+
+        // 无法使用泛型创建数组
+        // T[] arr = new T[];
+
+        // 无法针对泛型执行实例判断
+        // if(t instanceof T){
+        //     ...
+        // }
+    }
+
+    T getTarget() {
+        return t;
+    }
+
+    public static void main(String[] args) {
+        SimpleGeneric<String> strSg = new SimpleGeneric<>("hello");
+        String str = strSg.getTarget();
+    }
 }
